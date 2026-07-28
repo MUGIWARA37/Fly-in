d73050e7f765d6cb205c59a42fbd2c62204975c5
Rida Hlou <rhlou@e3r3p12.1337.ma>
2026-07-28 18:09:58 +0100

    models needed are finished

--- src/model/drone.py
+++ src/model/drone.py
@@ -1,5 +1,5 @@
 class Drone:
-    def __init__(self, drone_id: int, current_zone: str):
+    def __init__(self, drone_id: int, current_zone: str) -> None:
         self.drone_id = drone_id
         self.current_zone = current_zone
         self.status = "waiting"
